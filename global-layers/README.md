66c23509f9db6f07a5e7905cc8c94d896eea4612
Sean Tyler Myers <seamyers@amazon.com>
2019-04-23 18:01:11 -0700

    Initial commit

---
+++ global-layers/README.md
@@ -0,0 +1,3 @@
+### Problem
+
+User stated that global layers, once deleted, means you can't invoke that function any more. That's untrue, but reproducibility is the best proof of negation, so here we go.
---
+++ global-layers/app/app.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"os"
+	"strconv"
+	"github.com/aws/aws-lambda-go/lambda"
+
+)
+
+type Response struct {
+	Output string
+}
+
+func HandleRequest(ctx context.Context) (Response, error) {
+	res, err := os.Stat("/opt/example.txt")
+	if err != nil {
+		return Response {
+			Output: err.Error(),
+		}, nil
+	}
+
+	return Response{ Output: strconv.FormatInt(res.Size(), 10)}, nil
+}
+
+func main(){
+	lambda.Start(HandleRequest)
+}
