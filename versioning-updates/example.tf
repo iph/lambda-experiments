2401d21902569956f093fed5233ab5df1c883aa0
Sean Tyler Myers <seamyers@amazon.com>
2020-03-20 00:14:20 -0700

    Remove publish so versioning wont be auto enabled

--- versioning-updates/example.tf
+++ versioning-updates/example.tf
@@ -26,9 +26,8 @@ EOF
 resource "aws_lambda_function" "test_lambda" {
   filename      = "app.zip"
   function_name = "lambda-example-versioning-function"
-  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  role          = aws_iam_role.iam_for_lambda.arn
   handler       = "app"
-  publish       = true
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
